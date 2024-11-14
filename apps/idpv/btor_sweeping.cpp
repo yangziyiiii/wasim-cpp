90440926b4e07a42f9ce65e26d6214edce01d90a
yangziyiiii <2420769218@qq.com>
2024-11-14 10:24:39 +0800

    todo: function bv->btorbitvector

--- apps/idpv/btor_sweeping.cpp
+++ apps/idpv/btor_sweeping.cpp
@@ -12,80 +12,11 @@
 
 #include <iomanip>
 #include <chrono>
+#include "btor_sweeping.h"
 
 using namespace smt;
 using namespace std;
 using namespace wasim;
-
-// void post_order_traversal(const Term& term, std::vector<Term>& post_order_list) {
-//     assert(term != nullptr);
-
-//     std::unordered_set<Term> visited_nodes;
-//     std::stack<Term> node_stack;
-//     std::stack<Term> output_stack;
-
-//     node_stack.push(term);
-//     while (!node_stack.empty()) {
-//         Term current = node_stack.top();
-//         node_stack.pop();
-//         output_stack.push(current);
-//         visited_nodes.insert(current);
-
-//         std::vector<Term> children;
-//         for (auto child : current) {
-//             if (child && visited_nodes.find(child) == visited_nodes.end()) {
-//                 children.push_back(child);
-//             }
-//         }
-
-//         for (int i = children.size() - 1; i >= 0; --i) {
-//             node_stack.push(children[i]);
-//         }
-//     }
-
-//     while (!output_stack.empty()) {
-//         Term current = output_stack.top();
-//         output_stack.pop();
-//         post_order_list.push_back(current);
-//     }
-
-// }
-
-void post_order_traversal(const Term& term, std::vector<Term>& post_order_list) {
-    std::stack<Term> node_stack;
-    std::stack<Term> output_stack;
-    std::unordered_set<Term> visited;
-
-    node_stack.push(term);
-    while(!node_stack.empty()){
-        Term current = node_stack.top();
-
-        if( current->is_symbol() || 
-            current->is_value() || 
-            (current->get_op().is_null() && !current->is_symbolic_const()) ||
-            current -> get_sort() -> get_sort_kind() == ARRAY){
-            node_stack.pop();
-        }
-        
-        node_stack.pop();
-        output_stack.push(current);
-        visited.insert(current);
-
-        for(auto child : current){
-            if(child && visited.find(child) == visited.end())
-                node_stack.push(child);
-        }
-    }
-
-    while(!output_stack.empty()){
-        Term current = output_stack.top();
-        output_stack.pop();
-        post_order_list.push_back(current);
-    }
-}
-
-
-
 
 std::chrono::time_point<std::chrono::high_resolution_clock> last_time_point;
 void print_time() {
@@ -102,7 +33,6 @@ int main() {
     auto start_time = std::chrono::high_resolution_clock::now();
 
     SmtSolver solver = BoolectorSolverFactory::create(false);
-    // SmtSolver solver = BitwuzlaSolverFactory::create(false);
 
     solver->set_logic("QF_UFBV");
     solver->set_opt("incremental", "true");
@@ -142,22 +72,38 @@ int main() {
 
     auto root = solver->make_term(Equal, a_output_term, b_output_term);
 
+    //start post order traversal
+    std::stack<Term> node_stack;
+    node_stack.push(root);
+    std::unordered_map<Term, bool> nodes;
+    nodes.insert({root, false});
+
+    while(!node_stack.empty()) {
+        Term current = node_stack.top();
+        for(Term child : current) {
+            if(child){ // 子节点存在，继续向下遍历
+                nodes[current] = true;
+                if(nodes.find(child) == nodes.end()){
+                    node_stack.push(child);
+                    nodes.insert({child, false});
+                }
+            }
+            else{ // 子节点不存在，对当前节点进行simulation
+                if(child->is_value()) { // 常量
+                    btor_bv_const();
+                }
+                else if(child->is_symbol()) { // 变量
+                    
+                    if(child->get_op() == BVAdd) {
+
+                    }
+                }
+            }
+        }
+    }
+    
 
 
-
-
-
-
-    //post order traversal
-    std::vector<Term> post_order_list;
-    post_order_traversal(root, post_order_list);
-    cout << "post order traversal size: " << post_order_list.size() << endl;
-
-    smt::UnorderedTermMap substitution_map;
-
-    // for(auto term : post_order_list) {
-    //     //TODO: new simulation method for a term using boolector
-    // }
 
 
 
@@ -166,3 +112,238 @@ int main() {
     return 0;
 
 }
+
+
+
+
+
+// template <typename T, typename... Rest>
+// inline void hashCombine(std::size_t & seed, T const & v, Rest &&... rest)
+// {
+//   std::hash<T> hasher;
+//   seed ^= hasher(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+//   (int[]){ 0, (hashCombine(seed, std::forward<Rest>(rest)), 0)... };
+// }
+
+
+// struct NodeData
+// {
+//   Term term;  // will be nullptr if it is for a term with array sort
+//   uint64_t bit_width;
+//   std::vector<std::string> simulation_data;
+
+//   NodeData() : term(nullptr), bit_width(0) {}
+
+//  private:
+//   NodeData(Term t, uint64_t bw) : term(t), bit_width(bw) {}
+
+//  public:
+//   void extend_val(SmtSolver & solver)
+//   {
+//     if (term == nullptr) return;  // array
+
+//     static std::string valstr;
+//     valstr.clear();
+//     Term value = solver->get_value(term);
+//     valstr = value->to_string();
+//     if (valstr == "true")
+//       valstr = "#b1";
+//     else if (valstr == "false")
+//       valstr = "#b0";
+//     simulation_data.push_back(std::move(valstr));
+//   }
+
+//   size_t hash() const
+//   {
+//     if (simulation_data.empty()) {
+//       return 0;
+//     }
+
+//     size_t hash_val = 0;
+//     for (const auto & v : simulation_data) {
+//       // Remove any prefix like "#b" before hashing
+//       std::string clean_val = v;
+//       if (v.substr(0, 2) == "#b") {
+//         clean_val = v.substr(2);
+//       }
+//       hashCombine(hash_val, clean_val);
+//     }
+//     return hash_val;
+//   }
+
+//   bool operator==(const NodeData & other) const
+//   {
+//     if (term == nullptr) return false;  // for array
+//     return simulation_data == other.simulation_data;
+//   }
+
+//   static NodeData from_term(const Term & term)
+//   {
+//     SortKind sk = term->get_sort()->get_sort_kind();
+//     switch (sk) {
+//       case ARRAY: return NodeData(nullptr, 0);
+//       case BV: return NodeData(term, term->get_sort()->get_width());
+//       case BOOL: return NodeData(term, 1);
+//       default:
+//         throw std::invalid_argument("Unsupported sort: " + term->get_sort()->to_string());
+//     }
+//   }
+// };
+
+// void collect_terms(const Term & term, std::unordered_map<Term, NodeData> & node_data_map)
+// {
+//     if (!term) {
+//         throw std::invalid_argument("Null term provided to collect_terms");
+//     }
+
+//     std::unordered_set<Term> visited_nodes;
+//     std::stack<Term> node_stack;
+//     node_stack.push(term);
+
+//     while (!node_stack.empty()) {
+//         Term current_term = node_stack.top();
+//         node_stack.pop();
+//         if (visited_nodes.find(current_term) != visited_nodes.end())
+//             continue;
+
+//         // early pruning
+//         if (current_term->is_value() || current_term->is_symbol()
+//             || (current_term->get_op().is_null()
+//             && !current_term->is_symbolic_const())) {
+//             // Add sort-based pruning
+//             Sort s = current_term->get_sort();
+//             if (s->get_sort_kind() == ARRAY) {
+//                 continue;  // Skip array terms early
+//             }
+//             continue;
+//         }
+
+//         visited_nodes.insert(current_term);
+//         auto res = node_data_map.emplace(current_term, NodeData::from_term(current_term));
+//         assert(res.second);
+
+//         if (res.second) {  // Only process children if this is a new term
+//             for (auto child : current_term) {
+//                 if (child) {
+//                 node_stack.push(child);
+//                 }
+//             }
+//         }
+//     }
+// }
+
+// void collect_termdata(SmtSolver & solver, std::unordered_map<Term, NodeData> & node_data_map)
+// {
+//     for (auto & term_data_pair : node_data_map) {
+//         term_data_pair.second.extend_val(solver);
+//     }
+// }
+
+// class GmpRandStateGuard
+// {
+//     gmp_randstate_t state;
+
+//     public:
+//     GmpRandStateGuard()
+//     {
+//         gmp_randinit_default(state);
+//         gmp_randseed_ui(state, time(NULL));
+//     }
+
+//     ~GmpRandStateGuard() { gmp_randclear(state); }
+
+//     void random_128(mpz_t & rand_num)
+//     {
+//         mpz_init2(rand_num, 128);
+//         mpz_urandomb(rand_num, state, 128);
+//     }
+
+//     operator gmp_randstate_t &() { return state; }
+// };
+
+// void post_order_traversal(const Term& term, std::vector<Term>& post_order_list) {
+//     std::stack<Term> node_stack;
+//     std::stack<Term> output_stack;
+//     std::unordered_set<Term> visited;
+
+//     node_stack.push(term);
+//     while(!node_stack.empty()){
+//         Term current = node_stack.top();
+
+//         if( current->is_symbol() || 
+//             current->is_value() || 
+//             (current->get_op().is_null() && !current->is_symbolic_const()) ||
+//             current -> get_sort() -> get_sort_kind() == ARRAY){
+//             node_stack.pop();
+//         }
+        
+//         node_stack.pop();
+//         output_stack.push(current);
+//         visited.insert(current);
+
+//         for(auto child : current){
+//             if(child && visited.find(child) == visited.end())
+//                 node_stack.push(child);
+//         }
+//     }
+
+//     while(!output_stack.empty()){
+//         Term current = output_stack.top();
+//         output_stack.pop();
+//         post_order_list.push_back(current);
+//     }
+// }
+
+
+// std::unordered_map<Term, NodeData> node_data_map;
+    // collect_terms(root, node_data_map);
+
+    // //post order traversal
+    // std::vector<Term> post_order_list;
+    // post_order_traversal(root, post_order_list);
+    // print_time();
+    // cout << "post order traversal size: " << post_order_list.size() << endl;
+
+    // // simulation loop
+    // GmpRandStateGuard rand_guard;
+    // int num_iterations = 1;//FIXME:
+    // for (int i = 0; i < num_iterations; ++i) {
+    //     print_time();
+    //     cout << "Running " << i + 1 << " simulation iteration...\n";
+    //     solver->push(); // push for each iteration - context level 2
+    //     mpz_t key_mpz, input_mpz;
+    //     rand_guard.random_128(key_mpz);
+    //     rand_guard.random_128(input_mpz);
+
+    //     // Use RAII for GMP strings
+    //     unique_ptr<char, void (*)(void *)> key_str(mpz_get_str(NULL, 10, key_mpz), free);
+    //     unique_ptr<char, void (*)(void *)> input_str(mpz_get_str(NULL, 10, input_mpz), free);
+
+    //     auto input_val = solver->make_term(key_str.get(), a_key_term->get_sort());
+    //     auto key_val = solver->make_term(input_str.get(), a_input_term->get_sort());
+
+    //     TermVec assumptions{ solver->make_term(Equal, a_key_term, key_val),
+    //                         solver->make_term(Equal, a_input_term, input_val),
+    //                         solver->make_term(Equal, b_key_term, key_val),
+    //                         solver->make_term(Equal, b_input_term, input_val) };
+    //     auto check_result = solver->check_sat_assuming(assumptions);
+    //     if (!check_result.is_sat()) {
+    //         throw std::runtime_error("Unexpected UNSAT result during simulation");
+    //     }
+
+    //     collect_termdata(solver, node_data_map);
+    //     solver->pop();  // Restore to base context - context level 1
+
+    //     mpz_clear(key_mpz);
+    //     mpz_clear(input_mpz);
+    // }  // end of simulation
+    // cout << "Simulation complete.\n";
+
+    // print_time();
+    // cout << "node_data_map size : " << node_data_map.size() << endl;
+
+    // solver->push(); // save the final state - context level 2
+    // solver->assert_formula(solver->make_term(smt::Equal, a_key_term, b_key_term));
+    // solver->assert_formula(solver->make_term(smt::Equal, a_input_term, b_input_term));
+
+    // std::unordered_map<Term, Term> substitution_map;
---
+++ apps/idpv/btor_sweeping.h
@@ -0,0 +1,179 @@
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <assert.h>
+#include <stdlib.h>
+#include "gmp.h"
+#include "gmpxx.h"
+
+#define BTOR_BV_TYPE uint32_t
+#define BTOR_BV_TYPE_BW (sizeof (BTOR_BV_TYPE) * 8)
+
+struct BtorBitVector
+{
+  uint32_t width; /* length of bit vector */
+#ifdef BTOR_USE_GMP
+  mpz_t val;
+#else
+  uint32_t len;   /* length of 'bits' array */
+
+  /* 'bits' represents the bit vector in 32-bit chunks, first bit of 32-bit bv
+   * in bits[0] is MSB, bit vector is 'filled' from LSB, hence spare bits (if
+   * any) come in front of the MSB and are zeroed out.
+   * E.g., for a bit vector of width 31, representing value 1:
+   *
+   *    bits[0] = 0 0000....1
+   *              ^ ^--- MSB
+   *              |--- spare bit
+   * */
+  BTOR_BV_TYPE bits[];
+#endif
+};
+
+struct BtorMemMgr
+{
+  size_t allocated;
+  size_t maxallocated;
+  size_t sat_allocated;
+  size_t sat_maxallocated;
+};
+
+typedef struct BtorMemMgr BtorMemMgr;
+
+typedef struct BtorBitVector BtorBitVector;
+
+static uint32_t hash_primes[] = {333444569u, 76891121u, 456790003u};
+#define NPRIMES ((uint32_t) (sizeof hash_primes / sizeof *hash_primes))
+
+uint32_t btor_bv_hash (const BtorBitVector *bv) {
+    assert(bv);
+    uint32_t i, j = 0, n, res = 0;
+  uint32_t x, p0, p1;
+
+  res = bv->width * hash_primes[j++];
+
+#ifdef BTOR_USE_GMP
+  // least significant limb is at index 0
+  mp_limb_t limb;
+  for (i = 0, j = 0, n = mpz_size (bv->val); i < n; ++i)
+  {
+    p0 = hash_primes[j++];
+    if (j == NPRIMES) j = 0;
+    p1 = hash_primes[j++];
+    if (j == NPRIMES) j = 0;
+    limb = mpz_getlimbn (bv->val, i);
+    if (mp_bits_per_limb == 64)
+    {
+      uint32_t lo = (uint32_t) limb;
+      uint32_t hi = (uint32_t) (limb >> 32);
+      x           = lo ^ res;
+      x           = ((x >> 16) ^ x) * p0;
+      x           = ((x >> 16) ^ x) * p1;
+      x           = ((x >> 16) ^ x);
+      p0          = hash_primes[j++];
+      if (j == NPRIMES) j = 0;
+      p1 = hash_primes[j++];
+      if (j == NPRIMES) j = 0;
+      x = x ^ hi;
+    }
+    else
+    {
+      assert (mp_bits_per_limb == 32);
+      x = res ^ limb;
+    }
+    x   = ((x >> 16) ^ x) * p0;
+    x   = ((x >> 16) ^ x) * p1;
+    res = ((x >> 16) ^ x);
+  }
+#else
+  for (i = 0, j = 0, n = bv->len; i < n; i++)
+  {
+    p0 = hash_primes[j++];
+    if (j == NPRIMES) j = 0;
+    p1 = hash_primes[j++];
+    if (j == NPRIMES) j = 0;
+    x   = bv->bits[i] ^ res;
+    x   = ((x >> 16) ^ x) * p0;
+    x   = ((x >> 16) ^ x) * p1;
+    res = ((x >> 16) ^ x);
+  }
+#endif
+  return res;
+}
+
+BtorBitVector * btor_bv_new (BtorMemMgr *mm, uint32_t bw)
+{
+  assert (mm);
+  assert (bw > 0);
+
+  BtorBitVector *res;
+
+#ifdef BTOR_USE_GMP
+  BTOR_NEW (mm, res);
+  res->width = bw;
+  mpz_init (res->val);
+#else
+  uint32_t i;
+
+  i = bw / BTOR_BV_TYPE_BW;
+  if (bw % BTOR_BV_TYPE_BW > 0) i += 1;
+
+  assert (i > 0);
+  res =
+      btor_mem_malloc (mm, sizeof (BtorBitVector) + sizeof (BTOR_BV_TYPE) * i);
+  BTOR_CLRN (res->bits, i);
+  res->len = i;
+  assert (res->len);
+  res->width = bw;
+  assert (res->width <= res->len * BTOR_BV_TYPE_BW);
+#endif
+  return res;
+}
+
+BtorBitVector *
+btor_bv_new_random (BtorMemMgr *mm, BtorRNG *rng, uint32_t bw)
+{
+  BtorBitVector *res;
+#ifdef BTOR_USE_GMP
+  res = btor_bv_new (mm, bw);
+  mpz_urandomb (res->val, *((gmp_randstate_t *) rng->gmp_state), bw);
+  mpz_fdiv_r_2exp (res->val, res->val, bw);
+#else
+  res = btor_bv_new_random_bit_range (mm, rng, bw, bw - 1, 0);
+#endif
+  return res;
+}
+
+bool btor_util_check_bin_to_bv (BtorMemMgr *mm, const char *str, uint32_t bw)
+{
+  assert (mm);
+  assert (str);
+  assert (bw);
+
+  (void) mm;
+  return strlen (str) <= bw;
+}
+
+
+BtorBitVector *btor_bv_const(BtorMemMgr *mm, const char *str, uint32_t bw){
+    assert (btor_util_check_bin_to_bv (mm, str, bw));
+
+    BtorBitVector *res;
+
+#ifdef BTOR_USE_GMP
+    BTOR_NEW (mm, res);
+    res->width = bw;
+    mpz_init_set_str (res->val, str, 2);
+#else
+    uint32_t i, j, bit;
+    res = btor_bv_new (mm, bw);
+    for (i = 0; i < bw; i++)
+    {
+        j = bw - 1 - i;
+        assert (str[j] == '0' || str[j] == '1');
+        bit = str[j] == '0' ? 0 : 1;
+        btor_bv_set_bit (res, i, bit);
+    }
+#endif
+    return res;
+}
